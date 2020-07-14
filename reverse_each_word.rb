e027da4508a0843c0a96375c6759a5e7cfd1dfac
Steve Cordova <cordovas@bc.edu>
2020-07-14 13:59:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,22 @@
-def reverse_each_word(sentence)
-  a = sentence.reverse
-  x = a.split(" ")
-  y = []
-  x.collect do |name|
-    y.unshift(name)
-  end
-  y.join(" ")
+# def reverse_each_word(sentence)
+#   a = sentence.reverse
+#   x = a.split(" ")
+#   y = []
+#   x.collect do |name|
+#     y.unshift(name)
+#   end
+#   y.join(" ")
+# end
+
+
+reverse_each_word("Hello there, and how are you?")
+  #=> "olleH ,ereht dna woh era ?uoy"
+def reverse_each_word(phrase)
+  reverse_array = []
+new_array = phrase.split(" ") #["hello", "there,", "and" ...]
+  new_array.each do |word|
+      reverse_array <<  word.reverse
 end
+reverse_array.join(" ")
+end
+  
